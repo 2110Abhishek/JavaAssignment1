c7a7db701849f6360f89404ad1a1affaf99fb566
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-11 15:08:39 +0530

    Abhishek

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Team5 team1=new Team5();
+        team1.name();
+
+    }
+}
---
+++ src/Team5.java
@@ -0,0 +1,10 @@
+public class Team5 {
+    public void name(){
+        System.out.println("Abhishek");
+        System.out.println("Shreyash");
+        System.out.println("Ratan");
+        System.out.println("Astik");
+        System.out.println("Ram");
+    }
+
+}
